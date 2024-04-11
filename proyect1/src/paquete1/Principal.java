2b36f4797d2118cd9d01e844a34c86f5e1bf8203
erikd <erikd@192.168.1.107>
2024-04-11 10:18:25 +0200

    cuarta confirmacion

--- proyect1/src/paquete1/Principal.java
+++ proyect1/src/paquete1/Principal.java
@@ -2,7 +2,6 @@ package paquete1;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Estamos haciendo control de versiones con Git");
 		System.out.println("Estamos haciendo control de versiones con Git 2º version");
 		System.out.println("Estamos haciendo control de versiones con Git 3º version");
 		}
